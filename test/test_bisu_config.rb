30b5847e6eb8f40bab10046fa7298e61acb5c165
João Costa <joaostacosta@gmail.com>
2015-07-01 09:51:18 +0100

    Reviews config file format to support RoR

--- test/test_bisu_config.rb
+++ test/test_bisu_config.rb
@@ -6,18 +6,21 @@ class BisuConfigTest < Minitest::Test
   def test_parse
     config = Bisu::Config.parse("test/support/sample_translatable.yml")
 
-    assert_equal config[:type], "iOS"
+    assert_equal config[:type], "BisuOS"
     assert_equal config[:sheet_id], "abc1234567890"
     assert_equal config[:keys_column], "key_name"
     
     assert_equal config[:in], [
-      "folder/file_1.translatable",
-      "folder/file_2.translatable"
+      "path/to/file/to/1.ext.translatable",
+      "path/to/file/to/2.ext.translatable"
     ]
     
+    assert_equal config[:out_path], "path/to/final-%{locale}/%{out_name}"
+
     assert_equal config[:out], [
-      { language: "english", folder: "hole19/en.lproj/" },
-      { language: "portuguese", folder:   "hole19/pt.lproj/" }
+      { locale: "en",    kb_language: "english", path: "path/to/default/%{out_name}" },
+      { locale: "pt",    kb_language: "portuguese" },
+      { locale: "pt-PT", kb_language: "portuguese" }
     ]
   end
 end
