336e8a54843dcf42694098678b0bed7459a9f002
João Costa <joao.costa@hole19golf.com>
2016-10-21 23:33:33 +0100

    Converts Translator tests to spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,8 @@ RSpec.configure do |config|
     Bisu::Logger.clean_summary
   end
 
+  RSpec::Matchers.define_negated_matcher :not_change, :change
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
