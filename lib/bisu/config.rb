1c876d54add01ab922fdb4bfe8953b92062219c5
João Costa <joao.costa@hole19golf.com>
2016-10-27 04:03:16 +0100

    Adds a method for validating the config hash structure

--- lib/bisu/config.rb
+++ lib/bisu/config.rb
@@ -1,8 +1,8 @@
 module Bisu
   class Config
     def initialize(hash:)
-      raise ArgumentError.new("hash: expected Hash, got #{hash.class}") unless hash.is_a? Hash
       @hash = deep_sym(hash)
+      validate_structure!(@hash)
     end
 
     def to_h
@@ -18,6 +18,56 @@ module Bisu
         obj.inject({}) { |memo, (k,v)| memo[k.to_sym] = deep_sym(v); memo }
       else
         obj
+      end
+    end
+
+    VALID_STRUCTURE = {
+      type: Hash,
+      keys: {
+        type: { type: String },
+        sheet_id: { type: String },
+        keys_column: { type: String },
+        in: { type: Array, elements: {
+          type: String
+        } },
+        out_path: { type: String },
+        out: { type: Array, elements: {
+          type: Hash,
+          keys: {
+            locale: { type: String },
+            kb_language: { type: String },
+            path: { type: String, mandatory: false }
+          }
+        } }
+      }
+    }
+
+    def validate_structure!(value, structure=VALID_STRUCTURE, error_prefix=["value"])
+      if value == nil && structure.key?(:mandatory) && !structure[:mandatory]
+        return
+      end
+
+      prepend_error = "Invalid structure! #{error_prefix.join}:"
+
+      unless value.is_a? structure[:type]
+        raise ArgumentError.new("#{prepend_error} Expected #{structure[:type]}, got #{value.class}")
+      end
+
+      case value
+      when Array
+        value.each_with_index do |elem, i|
+          validate_structure!(elem, structure[:elements], error_prefix + ["[#{i}]"])
+        end
+      when Hash
+        mandatory_keys = structure[:keys].map { |k,s| k unless s.key?(:mandatory) && !s[:mandatory] }.compact
+
+        unless (missing = mandatory_keys - value.keys).empty?
+          raise ArgumentError.new("#{prepend_error} Missing keys: #{missing.join(', ')}")
+        end
+
+        structure[:keys].each do |key, structure|
+          validate_structure!(value[key], structure, error_prefix + ["[:#{key}]"])
+        end
       end
     end
   end
--- spec/lib/bisu/config_spec.rb
+++ spec/lib/bisu/config_spec.rb
@@ -19,10 +19,30 @@ describe Bisu::Config do
     } }
 
     its(:to_h) { should eq(hash) }
+
+    context "when missing mandatory keys" do
+      let(:hash) { {} }
+      it { expect { config }.to raise_error /missing keys: type, sheet_id, keys_column, in, out_path, out/i }
+    end
+
+    context "when some String key has an invalid type" do
+      before { hash[:type] = {} }
+      it { expect { config }.to raise_error /expected string, got hash/i }
+    end
+
+    context "when the child of some Array key is invalid" do
+      before { hash[:in] = [{}] }
+      it { expect { config }.to raise_error /expected string, got hash/i }
+    end
+
+    context "when the child of some Hash key is invalid" do
+      before { hash[:out] = [{}] }
+      it { expect { config }.to raise_error /missing keys: locale, kb_language/i }
+    end
   end
 
   context "given something other then an Hash" do
     let(:hash) { nil }
-    it { expect { config }.to raise_error /expected Hash/ }
+    it { expect { config }.to raise_error /expected hash/i }
   end
 end
